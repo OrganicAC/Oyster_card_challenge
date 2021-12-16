d0121c05c4e0174afeee845ff44bef2abd4c1960
OrganicAC <anthonychaplin17@gmail.com>
2021-12-16 16:18:21 +0000

    Added station file and spec

---
+++ lib/station.rb
@@ -0,0 +1,10 @@
+require 'oyster'
+
+class Station 
+  attr_reader :name, :zone
+
+  def initialize(name, zone)
+    @name = name
+    @zone = zone
+  end 
+end 
---
+++ spec/station_spec.rb
@@ -0,0 +1,13 @@
+require 'station'
+
+describe Station do
+  subject {described_class.new("Euston", 2)}
+  it 'Returns station name' do
+    expect(subject.name).to eq "Euston" 
+  end 
+  it 'Returns station zone' do
+    expect(subject.zone).to eq 2
+  end
+end 
+ 
+#station name and station zone variable
